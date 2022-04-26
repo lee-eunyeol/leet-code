efccbf9e9fd565067e9fcb044eae46783cf94620
lee-eunyeol <61894450+lee-eunyeol@users.noreply.github.com>
2022-04-26 18:01:27 +0900

    Time: 236 ms (29.39%), Space: 49.4 MB (27.83%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.ts
+++ 13-roman-to-integer/13-roman-to-integer.ts
@@ -1,20 +1,38 @@
-function romanToInt(s: string): number {
-  const toNumber = {
-    ['I']: 1,
-    ['V']: 5,
-    ['X']: 10,
-    ['L']: 50,
-    ['C']: 100,
-    ['D']: 500,
-    ['M']: 1000,
-  };
-  const string = s.split('').map((a) => toNumber[a]);
-  let res = 0;
-  for (let i = 0; i < string.length; i++) {
-    const cur = string[i];
-    const next = string[i + 1] ?? 0;
-    if (cur < next) res -= cur;
-    else res += cur;
-  }
-  return res;
+interface Lengthwise {
+  length: number;
 }
+
+const CONVERSION_MAP = new Map<string, number>([
+  ["I", 1],
+  ["V", 5],
+  ["X", 10],
+  ["L", 50],
+  ["C", 100],
+  ["D", 500],
+  ["M", 1000],
+]);
+Object.freeze(CONVERSION_MAP);
+
+const isRomanNumeral = (roman: string): boolean => {
+  return roman.match(/[IVXLCDM]/g)?.length === roman.length;
+};
+
+const romanToInt = (roman: string): number => {
+  if (!roman || !isRomanNumeral(roman)) return 0;
+
+  let sum = 0;
+  const getLastIndex = <T extends Lengthwise>(t: T): number => t.length - 1;
+  for (let idx = 0; idx < getLastIndex(roman); idx++) {
+    const [romanChar1, romanChar2] = [roman.charAt(idx), roman.charAt(idx + 1)];
+    const backValue: number = CONVERSION_MAP.get(romanChar1)!;
+    const frontValue: number = CONVERSION_MAP.get(romanChar2)!;
+
+    if (backValue < frontValue) {
+      sum -= backValue;
+    } else {
+      sum += backValue;
+    }
+  }
+  sum += CONVERSION_MAP.get(roman.charAt(getLastIndex(roman)))!;
+  return sum;
+};
