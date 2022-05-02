d3769881a35cf60b762c36c1e0913ac264f2c883
lee-eunyeol <61894450+lee-eunyeol@users.noreply.github.com>
2022-05-02 17:21:32 +0900

    Time: 129 ms (43.08%), Space: 45.8 MB (26.92%) - LeetHub

---
+++ 905-sort-array-by-parity/905-sort-array-by-parity.ts
@@ -0,0 +1,12 @@
+function sortArrayByParity(nums: number[]): number[] {
+  const result: number[] = [];
+  const result2: number[] = [];
+  while (nums.length !== 0) {
+    if (nums[0] % 2 === 0) {
+      result.push(nums.shift());
+    } else {
+      result2.push(nums.shift());
+    }
+  }
+  return result.concat(result2);
+}
