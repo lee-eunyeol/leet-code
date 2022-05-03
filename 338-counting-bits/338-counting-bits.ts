3104c75680f300bbe91f0e8736406128fbb49df3
lee-eunyeol <61894450+lee-eunyeol@users.noreply.github.com>
2022-05-03 09:11:40 +0900

    Time: 106 ms (79.41%), Space: 51.4 MB (30.00%) - LeetHub

--- 338-counting-bits/338-counting-bits.ts
+++ 338-counting-bits/338-counting-bits.ts
@@ -1,17 +1,13 @@
-function countBits(n: number): number[] {
-  const res: number[] = [];
-  for (let i = 0; i <= n; i++) {
-    res.push(countChar(i.toString(2)));
-  }
-  return res;
-}
+const countBits = (n : number) => {
+  return Array.from({length: n + 1}, (v,i) => i).map(v => countChar(v.toString(2)))
+};
 
 function countChar(str: string): number {
-  let count = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (str.charAt(i) === '1') {
-      count++;
-    }
+let count = 0;
+for (let i = 0; i < str.length; i++) {
+  if (str[i] === '1') {
+    count++;
   }
-  return count;
+}
+return count;
 }
