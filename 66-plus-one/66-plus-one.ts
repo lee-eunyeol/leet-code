6f104e5beb473ebdd5500e5fb37c4e96cb0d9d59
lee-eunyeol <61894450+lee-eunyeol@users.noreply.github.com>
2022-05-04 11:18:58 +0900

    Time: 68 ms (84.26%), Space: 42.8 MB (73.11%) - LeetHub

--- 66-plus-one/66-plus-one.ts
+++ 66-plus-one/66-plus-one.ts
@@ -1,17 +1,3 @@
 function plusOne(digits: number[]): number[] {
-  let index = digits.length - 1;
-  while (index >= 0) {
-    const data = digits[index]+1;
-    if (data === 10) {
-      digits[index] = 0;
-      if (index === 0) {
-        digits.unshift(1);
-      }
-    } else {
-      digits[index] = data;
-      break;
-    }
-    index--;
-  }
-  return digits;
-}
+    return String(BigInt(digits.join('')) + 1n).split('').map(Number);
+};
