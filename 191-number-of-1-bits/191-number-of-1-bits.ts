7eaceabcd50e28e512b29bfe72f33c1717e84dcf
lee-eunyeol <61894450+lee-eunyeol@users.noreply.github.com>
2022-05-09 09:30:46 +0900

    Time: 75 ms (88.55%), Space: 44.9 MB (40.19%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.ts
+++ 191-number-of-1-bits/191-number-of-1-bits.ts
@@ -1,3 +1,15 @@
 function hammingWeight(n: number): number {
-    return n.toString(2).split('').filter(a => a==='1').length
-};
+  let count = 0;
+
+  if (n === 0) return 0;
+  let data = n;
+  while (data >= 1) {
+    if (data % 2 === 1) {
+      count++;
+      data = data / 2 - 0.5;
+    } else {
+      data = data / 2;
+    }
+  }
+  return count
+}
