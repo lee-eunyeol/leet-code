5ac3aff488163ee2233f109b242d8d78f07bfc94
lee-eunyeol <61894450+lee-eunyeol@users.noreply.github.com>
2022-05-12 14:24:00 +0900

    Time: 105 ms (54.89%), Space: 44.7 MB (70.68%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.ts
@@ -0,0 +1,9 @@
+function isPowerOfTwo(n: number): boolean {
+    if (n===1) return true
+    if(n<=0) return false
+    while(n > 1){
+        if(n % 2 === 1 ) return false
+        n = n / 2
+    }
+    return true
+};
