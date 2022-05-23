0882dc7ae278e7a439091fc83f0c623c76074dfa
lee-eunyeol <61894450+lee-eunyeol@users.noreply.github.com>
2022-05-23 12:50:22 +0900

    Time: 130 ms (41.73%), Space: 51.7 MB (80.25%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,11 @@
+function maxProfit(prices: number[]): number {
+  let min = Number.MAX_SAFE_INTEGER;
+  let max = 0;
+
+  prices.forEach((price) => {
+    min = Math.min(min, price);
+    max = Math.max(max, price - min);
+  });
+
+  return max;
+}
