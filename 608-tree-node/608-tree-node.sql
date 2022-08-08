3cb0c72ad8f9ab7ab6ecc697dc528c8e276d480f
lee-eunyeol <61894450+lee-eunyeol@users.noreply.github.com>
2022-08-08 14:45:45 +0900

    Time: 504 ms (53.40%), Space: 0B (100.00%) - LeetHub

---
+++ 608-tree-node/608-tree-node.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select id, 'Root' as type from Tree where p_id is null
+union
+select id, 'Leaf' as type from Tree where p_id is not null and id not in (select p_id from Tree where p_id is not null)
+union 
+select id, 'Inner' as type from Tree where p_id is not null and id in (select p_id from Tree where p_id is not null)
+order by id;
